3d7374ce6952a08bac2c3f9c21ab1b9af0d8b57e
Pauline-WN <ngurupauline0@gmail.com>
2024-09-18 08:48:50 +0300

    completed lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "Old"  # Default condition when shoe is created
+
+    # Getter and Setter for size with validation
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"  # Sets condition to "New" after repair
